a71e8cefa8e31a0f88cc0da50273c08a27e11604
Scott Finkelstein <finkelstein@adelphi.edu>
2023-04-11 22:58:12 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
+import os
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+template = """
+    Below is an email that may be poorly worded. Your goal is to:
+    - Properly format the email
+    - Convert the input text to a specified tone
+    - Convert the input text to a specified dialect
+
+    Here are some examples different Tones:
+    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you about.
+    - Informal: Went to Barcelona for the weekend. Lots to tell you.
+
+    Here are some examples of words in different dialects:
+    - American English: French Fries, cotton candy, apartment, garbage, cookie
+    - British English: chips, candyfloss, flat, rubbish
+
+    Below is the email, tone, and dialect:
+    TONE: {tone}
+    DIALECT: {dialect}
+    EMAIL: {email}
+
+    YOUR RESPONSE:
+"""
+
+prompt = PromptTemplate(
+    input_variables=['tone', 'dialect', 'email'],
+    template=template
+)
+
+def load_LLM():
+    llm = OpenAI(temperature=0.5)
+    return llm
+
+llm = load_LLM()
+
+st.set_page_config(page_title='Globalize Email', page_icon=':robot:')
+st.header('Globalize Text')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("Often professionals would like to improve their emails, but don't know how. Globalize Email will help you improve your email skills by converting your emails.")
+
+with col2:
+    st.image('firefly.jpg', width=500, caption="abstract")
+
+st.markdown('## Enter Your Email to Convert')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    option_tone = st.selectbox(
+        'Which tone would you like your email to have?',
+        ('Formal', 'Informal')
+    )
+with col2:
+    option_dialect = st.selectbox(
+        'Which English Dialect would you like?',
+        ('American English', 'British English')
+    )
+
+def get_text():
+    input_text = st.text_area(label='', placeholder='Your Email...', key='email_input')
+    return input_text
+
+
+email_input = get_text()
+
+st.markdown('## Your Converted Email:')
+
+if email_input:
+    
+    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+    formatted_email = llm(prompt_with_email)
+    st.write(formatted_email)
